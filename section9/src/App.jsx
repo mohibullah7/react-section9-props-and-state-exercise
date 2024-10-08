d30fe05a4b6e060f5a85040a35e13e77a21f630a
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-10-08 14:29:02 +0500

    worked

--- section9/src/App.jsx
+++ section9/src/App.jsx
@@ -6,16 +6,28 @@ import Navbar from './components/Navbar'
 
 function App() {
   
-  const arr= [1,2,4,5,6,3]
-
-  const [val , setVal] = useState(arr)
+  let data = [1,2,3,4,5,6,7,8]
+  const [val,setval]= useState(data)
+  
  
 
   return (
     <>
       <div className='h-screen w-[100%] bg-zinc-300  '>
         <Navbar ></Navbar>
+        {
+         val.map((item)=>{
+        return  <h1>{item}</h1>
+})
+        }
+
+        <button onClick={()=>(s)}
+         className='px-2 py-1 bg-blue-400'>CLick</button>
+
+       
+       
         {/* <Card ></Card> */}
+
       </div>
     </>
   )
