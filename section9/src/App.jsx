6f503c0606f2dd30af92b32a7e61ef957e4a2caa
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-10-08 18:00:21 +0500

    react today

--- section9/src/App.jsx
+++ section9/src/App.jsx
@@ -6,7 +6,12 @@ import Navbar from './components/Navbar'
 
 function App() {
   
-  let data = [1,2,3,4,5,6,7,8]
+  // let data = [1,2,3,4,5,6,7,8]
+  const data = [
+    {name:'khan',age:80,number :'no '},
+    {name:'nsam',age:32320,number :'nklo '},
+    {name:'lskalsk',age:20,number :'nsao '}
+  ]
   const [val,setval]= useState(data)
   
  
@@ -15,16 +20,20 @@ function App() {
     <>
       <div className='h-screen w-[100%] bg-zinc-300  '>
         <Navbar ></Navbar>
-        {
-         val.map((item)=>{
-        return  <h1>{item}</h1>
-})
-        }
+       {val.map((elem,index)=>{
+        
+        return<div>
+            <h1>{elem.name}</h1>
+            <h1>{elem.age}</h1>
+            <h1>{elem.number}</h1>
 
-        <button onClick={()=>(s)}
-         className='px-2 py-1 bg-blue-400'>CLick</button>
+        </div>
+        
 
-       
+       })}
+       <button className='px-2 py-2 bg-blue-300 ' onClick={()=>setval([...val,age:410])}>
+          click
+       </button>
        
         {/* <Card ></Card> */}
 
